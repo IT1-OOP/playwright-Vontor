7349f16e91f4e809b5c53f112ee014ef758c949b
Vontor <94050058+Vontor@users.noreply.github.com>
2025-04-03 13:11:31 +0200

    funguje

--- tests/test_page.py
+++ tests/test_page.py
@@ -1,1 +1,15 @@
-nadpis_2 = page.locator("text='Nadpis 2'")
+import os
+from playwright.sync_api import sync_playwright, expect
+import urllib
+
+def test_page_title():
+    cesta = os.path.abspath("index.html")
+    cesta = urllib.parse.unquote(cesta)
+
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=True)
+        page = browser.new_page()
+        page.goto(f"file:///{cesta}")
+        nadpis_1 = page.locator("h1").first
+        expect(nadpis_1).to_be_visible()
+        browser.close()
