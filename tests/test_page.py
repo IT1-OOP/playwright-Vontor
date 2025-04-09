e40da967279e8482d1fba313fa60e01cd36c3a3a
Vontor <94050058+Vontor@users.noreply.github.com>
2025-04-09 12:26:31 +0200

    9.4.2025

--- tests/test_page.py
+++ tests/test_page.py
@@ -10,6 +10,17 @@ def test_page_title():
         browser = p.chromium.launch(headless=True)
         page = browser.new_page()
         page.goto(f"file:///{cesta}")
+
         nadpis_1 = page.locator("h1").first
+        nadpis_text = page.locator('text="Nadpis 1"')
+        div_1 = page.locator('.container')
+
         expect(nadpis_1).to_be_visible()
+
+        expect(nadpis_text).to_be_visible()
+        expect(nadpis_text).to_have_count(1)
+
+        expect(div_1).to_be_visible()
+
+
         browser.close()
